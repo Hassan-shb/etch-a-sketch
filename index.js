45b27e8a05fc1379cb84c3f8bc024789ab1a361e
WaiYanHtun <waiyanhtunsince2000@gmail.com>
2023-11-24 09:36:05 +0630

    The implmentation of the etch-a-sketch is over

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 const container = document.querySelector('.container');
-
 let grid;
-
 function createAGrid(){
     grid = document.createElement('div');
     grid.setAttribute('class', 'colorGrid');
-    grid.style.background = 'pink';
-    grid.style.width = '16px';
+    grid.style.width = `16px`;
     grid.style.height = '16px';
     return grid;
 }
-function changeColor(item){
-    item.style.background = "red";
-    console.log('mouseClicked');
+for(let i = 0; i < 16 * 16; i++){
+    container.appendChild(createAGrid());
 }
-for(let i = 0; i < 16; i++){
-    for(let i = 0; i < 16; i++){
-        container.appendChild(createAGrid());
-    }
-}
-
-
 const div = document.querySelectorAll('.colorGrid');
-
-div.forEach(item => {
-    item.addEventListener('mouseover', (one) => {
-        one = item;
-        item.style.background = "red";
-    });
+let isDrawing = false;
+container.addEventListener('mousedown', () => {
+    isDrawing = true;
 });
+container.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+function changeColor(){
+    div.forEach(item => {
+        item.addEventListener('mousemove',(e) => {
+            if(isDrawing){
+                e.target.style.background = "pink";
+            } 
+        });
+        
+        
+    });
+}
+container.addEventListener('mousedown', changeColor);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
