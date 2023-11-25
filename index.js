8722228559615b0dc2a7f3e3988c8fb1477cfffe
WaiYanHtun <waiyanhtunsince2000@gmail.com>
2023-11-25 09:32:56 +0630

    Color picker is implemented and the first touch is paineted. Still need to work on the other features.

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
-const container = document.querySelector('.container');
-let grid;
-function createAGrid(){
-    grid = document.createElement('div');
-    grid.setAttribute('class', 'colorGrid');
-    grid.style.width = `16px`;
-    grid.style.height = '16px';
-    return grid;
-}
-for(let i = 0; i < 16 * 16; i++){
-    container.appendChild(createAGrid());
-}
-const div = document.querySelectorAll('.colorGrid');
+const container = document.querySelector(".container");
+const comStyle = window.getComputedStyle(container);
+let colorPicker = document.getElementById('myColor');
+let selectedColor = colorPicker.value;
+
 let isDrawing = false;
-container.addEventListener('mousedown', () => {
-    isDrawing = true;
-});
-container.addEventListener('mouseup', () => {
-    isDrawing = false;
-});
-function changeColor(){
-    div.forEach(item => {
-        item.addEventListener('mousemove',(e) => {
-            if(isDrawing){
-                e.target.style.background = "pink";
-            } 
-        });
-        
-        
+
+const defaultColor = "red";
+function createAGrid(item) {
+    let pixel = document.createElement("div");
+    let width = comStyle.getPropertyValue("width");
+    width = `${+width.slice(0, 3) / item}px`;
+    let height = width;
+    pixel.style.width = width;
+    pixel.style.height = height;
+    pixel.style.border = "0.5px solid gray";
+    pixel.style.background = "pink";
+    pixel.classList.add("colorGrid");
+    
+    pixel.addEventListener("mousedown", (e) => {
+        colorPicker.addEventListener('input', function() {
+            selectedColor = colorPicker.value;
+          });
+        e.target.style.background = selectedColor;
+        isDrawing = true;
+        changeColor();
+    });
+    pixel.addEventListener("mouseup", () => {
+        isDrawing = false;
+    });
+    
+    container.appendChild(pixel);
+}
+function createGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        createAGrid(size);
+    }
+}
+function changeColor() {
+    container.addEventListener("mouseover", (e) => {
+        if(isDrawing){
+            colorPicker.addEventListener('input', function() {
+                selectedColor = colorPicker.value;
+              });
+            e.target.style.background = selectedColor;
+        }
     });
 }
-container.addEventListener('mousedown', changeColor);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+createGrid(16);
 
