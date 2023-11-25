8709dcf1cb34d8d316d6dd7646300ca973894de3
WaiYanHtun <waiyanhtunsince2000@gmail.com>
2023-11-25 12:16:12 +0630

    Rainbow color drawing is implemented. Now implementing the colormode button

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const container = document.querySelector(".container");
 const comStyle = window.getComputedStyle(container);
 let colorPicker = document.getElementById('myColor');
+const myEraser = document.getElementById('myEraser');
+const rainbow = document.getElementById('Rainbow');
+let isRainbow = false;
+let rainbowColor;
 let selectedColor = colorPicker.value;
-
 let isDrawing = false;
-
 const defaultColor = "red";
 function createAGrid(item) {
     let pixel = document.createElement("div");
@@ -18,36 +20,54 @@ function createAGrid(item) {
     pixel.classList.add("colorGrid");
     
     pixel.addEventListener("mousedown", (e) => {
-        colorPicker.addEventListener('input', function() {
-            selectedColor = colorPicker.value;
-          });
-        e.target.style.background = selectedColor;
-        isDrawing = true;
-        changeColor();
+        if(myEraser.checked == true){
+            e.target.style.background = "pink";
+            isDrawing = true;
+            changeColor("pink");
+        }else if(rainbow.checked == true){
+            e.target.style.background = rainbowGenerator();
+            rainbowColor = rainbowGenerator();
+            changeColor(rainbowColor);
+            isDrawing = true;
+        }else{
+            colorPicker.addEventListener('input', function() {
+                selectedColor = colorPicker.value;
+            });
+            e.target.style.background = selectedColor;
+            isDrawing = true;
+            changeColor(selectedColor);
+        }  
+        
     });
     pixel.addEventListener("mouseup", () => {
         isDrawing = false;
+        changeColor();
     });
     
     container.appendChild(pixel);
+}
+
+function changeColor(color) {
+    container.addEventListener("mouseover", (e) => {
+        console.log(e.target);
+        if(isDrawing && rainbow.checked == true){
+            rainbowColor = rainbowGenerator();
+            e.target.style.background = rainbowColor;
+        }else if(isDrawing){
+            e.target.style.background = color;
+        }
+    });
+}
+function rainbowGenerator(){
+    let R = Math.floor(Math.random() * 256);
+    let G = Math.floor(Math.random() * 256);
+    let B = Math.floor(Math.random() * 256);
+    return `rgb(${R},${G}, ${B})`;
 }
 function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         createAGrid(size);
     }
 }
-function changeColor() {
-    container.addEventListener("mouseover", (e) => {
-        if(isDrawing){
-            colorPicker.addEventListener('input', function() {
-                selectedColor = colorPicker.value;
-              });
-            e.target.style.background = selectedColor;
-        }
-    });
-}
-
-
-
-createGrid(16);
+createGrid(20);
 
